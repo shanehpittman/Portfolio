ee9fc9a3c1f5aacb1a94f6579dae73e83b400095
shanehpittman <shanehpittman@gmail.com>
2019-03-21 19:12:22 -0400

    'js'

---
+++ dist/js/main.js
@@ -0,0 +1,29 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menuBtn = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-nav");
+const menuBtn = document.querySelector(".menu-branding");
+const menuBtn = document.querySelectorAll(".nav-items");
+
+var showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+
+    showMenu = false;
+  }
+}
